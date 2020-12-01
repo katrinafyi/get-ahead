4ebcdd06f48ee2934e46cb0efe9e3494a8d42b13
Kenton Lam <kenton_lam@outlook.com>
2020-12-01 11:54:32 +1000

    Fixed tree

--- tree.cpp
+++ tree.cpp
@@ -7,6 +7,14 @@ struct TreeNode {
     std::vector<TreeNode> children;
 };
 
+// intermediate result struct.
+struct Result {
+    // length of longest path.
+    int length;
+    // true if the longest path reaches the root node.
+    bool top;
+};
+
 #define TOP_PREFIX "+-"
 #define MID_PREFIX "| "
 #define BOT_PREFIX "  "
@@ -36,25 +44,47 @@ void print_tree(const TreeNode& node, const std::string& prefix) {
     }
 }
 
-int solve(const TreeNode& node) {
-    int m = 0;
+Result solve(const TreeNode& node) {
+    Result result = {0, true};
+
     for (const TreeNode& child : node.children) {
-        m = std::max(m,
-                (child.value == node.value + 1 ? 1 : 0)
-                + solve(child));
+        Result childResult = solve(child);
+    
+        // 2 options:
+        // - longest path is longest path of child, or
+        // - longest path is longest path of child + 1 if this node and child
+        //   are consecutive and child's longest path reaches the top.
+
+        int m = childResult.length;
+        bool top = false;
+
+        if (child.value == node.value + 1 && childResult.top) {
+            m = childResult.length + 1;
+            top = true;
+        }
+
+        // prioritise results with longer length OR top flag set.
+        if (m >= result.length && (m > result.length || top)) {
+            result.length = m;
+            result.top = top;
+        }
     }
-    return m;
+
+    // std::cout << node.value << ": " << result.length << ',' << result.top << std::endl;
+    return result;
 }
 
 int main(int argc, char** argv) {
     std::vector<TreeNode> tests = {
         {1, {{2, {{4}}}, {3}}},
-        {5, {{12}, {7, {{8, {{9, {{15}, {10}}}}}, {6}}}}}
+        {5, {{12}, {7, {{8, {{9, {{15}, {10}}}}}, {6}}}}},
+        {1, {{2, {{10, {{11, {{12, {{13, {{14}}}}}}}}}}}}},
     };
 
     for (const TreeNode& tree : tests) {
         print_tree(tree, "");
-        std::cout << "solution: " << 1 + solve(tree) << std::endl << std::endl;
+        Result result = solve(tree);
+        std::cout << "solution: " << 1 + result.length << std::endl << std::endl;
     }
 
     return 0;
